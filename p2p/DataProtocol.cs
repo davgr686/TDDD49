6a5e2c78f51e33f31b380eb017aca96efb76cf1e
Otto Denesfay <34476823+ottodenesfay@users.noreply.github.com>
2018-11-21 14:51:16 +0100

    DataProtocol not finished (json)

--- p2p/DataProtocol.cs
+++ p2p/DataProtocol.cs
@@ -1,14 +1,71 @@
 using System;
+using System.IO;
 using System.Collections;
 using System.Collections.Generic;
+using System.Web.UI;
+using System.Web.Script.Serialization;
+using Newtonsoft.Json;
 
 namespace DataProtocol
 {
     public class DataProtocol
     {
-        private String type;
-        private String name;
-        private String timestamp;
-        private String message;
+        private String _type;
+        private String _username;
+        private DateTime _timestamp = DateTime.Now;
+        private String _message;
+
+        public String Type
+        {
+            get
+            {
+                return _type;
+            }
+            set
+            {
+                //Maybe add checks for invalid input
+                _type = value;
+            }
+        }
+
+        public String Username
+        {
+            get
+            {
+                return _username;
+            }
+            set
+            {
+                //Maybe add checks for invalid input
+                _username = value;
+            }
+        }
+
+        public DateTime Timestamp
+        {
+            get
+            {
+                return _timestamp;
+            }
+            set;
+        }
+
+        public String Message
+        {
+            get
+            {
+                return _message;
+            }
+            set
+            {
+                //Maybe add checks for invalid input
+                _message = value;
+            }
+        }
+
+        public DataProtocol CreateDataProtocol()
+        {
+
+        }
     }
 }
