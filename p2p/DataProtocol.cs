72d141ca531eeb46997bcd3b935174f536ae3f34
Otto Denesfay <34476823+ottodenesfay@users.noreply.github.com>
2018-11-21 13:20:42 +0100

    Dataprotocol.cs created

---
+++ p2p/DataProtocol.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DataProtocol
+{
+    public class DataProtocol
+    {
+        private String type;
+        private String name;
+        private String timestamp;
+        private String message;
+    }
+}
